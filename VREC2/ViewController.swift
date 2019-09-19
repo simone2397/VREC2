2e8bf178007a72dcdc46854b7491e25a11e8de1a
Simone Grossi <simonegrossi@live.it>
2019-09-19 14:44:17 +0200

    Initial Commit

---
+++ VREC2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VREC2
+//
+//  Created by Simone Grossi on 19/09/2019.
+//  Copyright © 2019 Simone Grossi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
